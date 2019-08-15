3d701628667a0614bddda38cfb8ecbc6850e29f7
Josh Buckland <buck06191@gmail.com>
2019-08-15 15:49:35 +0100

    Upload Myriad config using SA gradient

---
+++ config_files/abc/myriad_neo021_gradient.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#$ -l h_rt=1:00:00
+#$ -N gradient_neo021
+#$ -o /home/ucbpjru/Scratch/neo021/out
+#$ -e /home/ucbpjru/Scratch/neo021/err
+#$ -wd /home/ucbpjru/Scratch
+# Set up the job array.  In this instance we have requested 1000 tasks
+# numbered 1 to 1000.
+#$ -t 1-40000
+
+module load python3/recommended
+cd $TMPDIR
+export BASEDIR="${HOME}/BayesCMD"
+
+DATASET="neo021"
+DATAFILE="${HOME}/neonatal-desat-modelling/data/formatted_data/${DATASET}_formatted.csv"
+CONFIGFILE="${HOME}/neonatal-desat-modelling/config_files/abc/neo_config_gradient.json"
+
+echo "Datafile is ${DATAFILE}\nConfig file is ${CONFIGFILE}."
+
+start=`date +%s`
+python3 $BASEDIR/scripts/batch.py 1000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
+echo "Duration: $(($(date +%s)-$start))" > $TMPDIR/$SGE_TASK_ID.timings.txt
+
+tar -zcvf $HOME/Scratch/${DATASET}/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz $TMPDIR
