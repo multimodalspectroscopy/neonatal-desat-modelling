75ca2e396d567807452a5dbd4eb13c224ca11124
Josh Buckland <buck06191@gmail.com>
2019-08-15 15:34:00 +0100

    Sensitivity analysis using gradient

--- bcmd-files/sensitivity/neo007_gradient.sh
+++ bcmd-files/sensitivity/neo007_gradient.sh
@@ -5,19 +5,19 @@ for DATASET in "${DATASETS[@]}"
 do
     printf "Working on dataset %s\n" "${DATASET}"
     # DATASET="neo007"
-    OUTDIR1=`readlink -m "../../data/SA_results/${DATASET}"`
-    OUTDIR2=`readlink -m "../../data/SA_results/${DATASET}_zero"`
+    OUTDIR1=`readlink -m "../../data/SA_results/gradient/${DATASET}"`
+    OUTDIR2=`readlink -m "../../data/SA_results/gradient/${DATASET}_zero"`
     
     WORKDIR=$1
-    JOBFILE1="${WORKDIR}${DATASET}_SA.dsimjob"
-    JOBFILE2="${WORKDIR}${DATASET}_SA_zero.dsimjob"
+    # JOBFILE1="${WORKDIR}${DATASET}_SA.dsimjob"
+    JOBFILE2="${WORKDIR}${DATASET}_gradient_SA_zero.dsimjob"
     
     DATAFILE="../../data/formatted_data/${DATASET}_formatted.csv"
     
     echo "Writing to ${OUTDIR1}"
-    mkdir -p ${OUTDIR1}
+    # mkdir -p ${OUTDIR1}
     mkdir -p ${OUTDIR2}
-    python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR1} -b ~/repos/Github/BayesCMD/build ${JOBFILE1} ${DATAFILE}
+    # python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR1} -b ~/repos/Github/BayesCMD/build ${JOBFILE1} ${DATAFILE}
     
     echo "Writing to ${OUTDIR2}"
     python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR2} -b ~/repos/Github/BayesCMD/build ${JOBFILE2} ${DATAFILE}
