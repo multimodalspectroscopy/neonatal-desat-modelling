5d713547c5dda70be58317ce9ee3141f68cbef98
Joshua Russell-Buckland <ucbpjru@login12.myriad.ucl.ac.uk>
2019-11-09 11:58:50 +0000

    Push cluster changes to remote for local check

--- config_files/single_run/run_single.sh
+++ config_files/single_run/run_single.sh
@@ -1,7 +1,21 @@
+#!/bin/bash -l
+#$ -l h_rt=00:10:00
+#$ -N gradient_neo007_debug
+#$ -o /home/ucbpjru/Scratch/neo007_gradient_debug/out
+#$ -e /home/ucbpjru/Scratch/neo007_gradient_debug/err
+#$ -wd /home/ucbpjru/Scratch/
+
+module load python3/recommended
+#Local2Scratch
+
+# cd $TMPDIR
+export BASEDIR="${HOME}/BayesCMD"
 DATASET="neo007"
-DATAFILE="/home/buck06191/repos/Github/neonatal-desat-modelling/data/formatted_data/${DATASET}_formatted.csv"
-CONFIG="/home/buck06191/repos/Github/neonatal-desat-modelling/config_files/single_run/${DATASET}.json"
+DATAFILE="${HOME}/neonatal-desat-modelling/data/formatted_data/${DATASET}_formatted.csv"
+CONFIG="${HOME}/neonatal-desat-modelling/config_files/single_run/${DATASET}.json"
 
 echo ${DATAFILE}
 echo ${CONFIG}
-python "/home/buck06191/repos/Github/BayesCMD/scripts/single_run/run_model.py" --workdir "/home/buck06191/repos/Github/neonatal-desat-modelling/data/debug_results/${DATASET}/" ${DATAFILE} ${CONFIG}
+python "${HOME}/BayesCMD/scripts/single_run/run_model.py" --workdir . ${DATAFILE} ${CONFIG}
+
+#tar -zcvf $HOME/Scratch/${DATASET}_gradient_debug/batch_$JOB_NAME.update.tar.gz $TMPDIR
